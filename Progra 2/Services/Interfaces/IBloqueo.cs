e463e20b4abb3924678e1661612128311c9a020e
Neil Vana <ran2511dy@gmail.com>
2026-06-09 15:13:12 -0600

    Añadi los dtos XD
    si

--- Progra 2/Services/Interfaces/IBloqueo.cs
+++ Progra 2/Services/Interfaces/IBloqueo.cs
@@ -1,10 +1,16 @@
-﻿using Entities;
-using Microsoft.Extensions.DependencyInjection;
+﻿using DTOs;
+using Entities;
 
 namespace Services.Interfaces
 {
     public interface Ibloqueo
     {
+        public List<Bloqueo> GetAllBloqueos();
+        public Bloqueo GetBloqueoById(int id);
+        public Bloqueo CreateBloqueoMesa(BloqueoMesaDto dto);
+        public Bloqueo CreateBloqueoZona(BloqueoZonaDto dto);
+        public bool VerificarBloqueo(int mesaId, DateTime fechaHora);
+        public Bloqueo DeleteBloqueo(int id);
 
     }
 }
